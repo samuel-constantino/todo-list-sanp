cb5b26d403e8c8ba87f3a948323672c5965865f7
Samuel Constantino <samuelicapuidados@gmail.com>
2022-04-14 10:54:47 -0300

    feat: implementa test unitário para camada de controle do registro de usuário.

---
+++ src/tests/unit/controllers/user/register.test.js
@@ -0,0 +1,39 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const userController = require('../../../../controllers/user');
+const userService = require('../../../../services/user');
+
+describe('testa camada de controle para registro de usuário', function () {
+    const req = {};
+    const res = {};
+
+    describe('sucesso', function () {
+        const NEW_USER_RESPONSE = {
+            id: '507f1f77bcf86cd799439011',
+            name: 'User Test',
+            email: 'test@test.com',
+        };
+        before(function () {
+            req.body = {
+                name: 'User Test',
+                email: 'test@test.com',
+                password: '12345678',
+            };
+
+            res.status = sinon.stub().returns(res);
+            res.json = sinon.stub().returns();
+            sinon.stub(userService, 'register').resolves(NEW_USER_RESPONSE);
+        });
+
+        it('retorna status 201 (created)', async function () {
+            await userController.register(req, res);
+            expect(res.status.calledWith(201)).to.be.equal(true);
+        });
+
+        it('retorna arquivo com formato json', async function () {
+            await userController.register(req, res);
+            expect(res.json.calledWith(NEW_USER_RESPONSE)).to.be.equal(true);
+        });
+    });
+});
