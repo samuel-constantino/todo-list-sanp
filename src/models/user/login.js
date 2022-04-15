d68b53af1b0ffb50535284b1e45788d06e1a8db5
Samuel Constantino <samuelicapuidados@gmail.com>
2022-04-15 10:17:48 -0300

    feat: implementa camada de modelo da rota de login

--- src/models/user/login.js
+++ src/models/user/login.js
@@ -1,5 +1,12 @@
-const login = () => (
-    console.log('not implemented')
-);
+const { connection } = require('../connection');
+
+const login = async ({ email, password }) => {
+    const db = await connection();
+    const userFound = await db.collection('users').findOne(
+        { email, password },
+        { projection: { password: false } },
+    );
+    return userFound;
+};
 
 module.exports = { login };
