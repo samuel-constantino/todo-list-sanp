ebc9f860867a6a9f6645cb158216a43dedc6d600
Samuel Constantino <samuelicapuidados@gmail.com>
2022-04-15 09:07:00 -0300

    feat: define rota para login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { registerRouter } = require('./registerRouter');
+const { loginRouter } = require('./loginRouter');
 const { error, swagger } = require('../middlewares');
 
 const router = express.Router({ mergeParams: true });
@@ -7,6 +8,7 @@ const router = express.Router({ mergeParams: true });
 router.use('/swagger', swagger.serve, swagger.setup);
 
 router.use('/register', registerRouter);
+router.use('/login', loginRouter);
 
 router.use(error);
 
---
+++ src/routes/loginRouter.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const rescue = require('express-rescue');
+const userController = require('../controllers/user');
+
+const loginRouter = express.Router();
+
+loginRouter.post('/', rescue(userController.login));
