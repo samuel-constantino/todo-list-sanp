0e9b4ee5926691c6fd6569b84906c25211fde736
Samuel Constantino <samuelicapuidados@gmail.com>
2022-04-15 10:05:00 -0300

    feat: implementa testes unitários para camada de serviço da rota de login

---
+++ src/tests/unit/services/user/login.test.js
@@ -0,0 +1,45 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const userService = require('../../../../services/user');
+const userModel = require('../../../../models/user');
+
+describe('Testa camada de serviço do login de usuário', function () {
+    let LOGIN_PAYLOAD = null;
+    let LOGIN_MODEL_RESPONSE = null;
+    const USER_PROPERTIES = ['id', 'name', 'email', 'token'];
+
+    describe('sucesso', function () {
+        LOGIN_PAYLOAD = {
+            email: 'test@test.com',
+            password: 'test123',
+        };
+        LOGIN_MODEL_RESPONSE = {
+            id: '507f1f77bcf86cd799439011',
+            name: 'User Test',
+            email: 'test@test.com',
+        };
+
+        let user = null;
+        before(async function () {
+            sinon.stub(userModel, 'findByEmail').resolves(true);
+            sinon.stub(userModel, 'login').resolves(LOGIN_MODEL_RESPONSE);
+            user = await userService.login(LOGIN_PAYLOAD);
+        });
+
+        after(function () {
+            userModel.login.resolves();
+            userModel.findByEmail.restore();
+        });
+
+        it('retorna um objeto', function () {
+            expect(user).to.be.an('object');
+        });
+
+        it('retorna um objeto com as propriedades corretas', function () {
+            USER_PROPERTIES.forEach((property) => {
+                expect(user).to.have.property(property);
+            });
+        });
+    });
+});
